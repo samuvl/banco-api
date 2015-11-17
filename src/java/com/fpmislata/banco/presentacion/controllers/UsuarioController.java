377b422df79075a37a9dc471cd4318b3d52c84a9
alumno <alumno@2A5PC15>
2015-11-17 09:11:37 +0100

    UsuarioController OK, falta probar que funcione

--- src/java/com/fpmislata/banco/presentacion/controllers/UsuarioController.java
+++ src/java/com/fpmislata/banco/presentacion/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  * @author samu_
  */
+@Controller
 public class UsuarioController {
     @Autowired 
     UsuarioService usuarioService;
